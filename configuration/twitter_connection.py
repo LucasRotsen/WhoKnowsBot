4302e386d4cba6ef1441b0df9eea19aa2f38d334
Lesandro Ponciano <lesandropcodes@users.noreply.github.com>
2019-04-19 17:46:40 -0300

    Removendo credenciais expostas publicamente

--- configuration/twitter_connection.py
+++ configuration/twitter_connection.py
@@ -1,10 +1,10 @@
 import twitter
 from twitter import error
 
-consumerKey = 'kaiZPz9QDQwf0cwK1tpkrrnfb'
-consumerSecret = 'CGjM0heOGIOaPhVum1IWwCLBEVcZt6uirchIgS3vBcuDlzUtsp'
-accessToken = '977978225870729216-d42TgvOQWerxGfmqjDWTMHCshsnf5UP'
-accessTokenSecret = 'aO5JfvApMwAGegjlHRYemkSCkqV7fTFYGQC3zleCB35Jq'
+consumerKey = ''
+consumerSecret = ''
+accessToken = ''
+accessTokenSecret = ''
 
 
 def open_connection():
