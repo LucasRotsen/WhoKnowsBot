b9865a9d4e626863c75d8da2770a6b82d4a9fe6e
Lucas Rotsen <lucasrotsen@gmail.com>
2019-09-04 00:36:36 -0300

    Remoção da dependência matplotlib

--- whoknowsbot-config.sh
+++ whoknowsbot-config.sh
@@ -11,8 +11,8 @@ else
 	sudo apt-get install python3.6
 
 	echo "Iniciando instalação do gerenciador de pacotes pip..."
-	sudo apt-get install python-pip
+	sudo apt-get install python3-pip
 
 	echo "Iniciando instalação das dependências do projeto..."
-	sudo python3.6 -m pip install python-twitter nltk wordcloud backoff matplotlib
+	sudo python3.6 -m pip install python-twitter nltk wordcloud backoff
 fi
